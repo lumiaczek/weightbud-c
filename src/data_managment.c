d5c6dd6a3af5a84c1f03f00d472487df46fcd56b
cukhi <mareksobkow21@gmail.com>
2026-04-20 11:22:07 +0200

    Przykładowy program obsługujący zarządzanie pamięcią (TO SIE PEWNIE ZMIENI)

---
+++ src/data_managment.c
@@ -0,0 +1,33 @@
+#include <data_managment.h>
+#include <storage.h>
+#include <gtk/gtk.h>
+
+GHashTable *memory_collection = NULL;
+void init_memory(){
+    memory_collection = g_hash_table_new_full(g_str_hash,g_str_equal,g_free,g_free);
+    printf("Memory Initialized. \n");
+}
+void ram_store_save(const char *key,gpointer data){
+    g_hash_table_insert(memory_collection,g_strdup(key),data);
+}
+
+void on_save_button_clicked(){
+    UserSettings *user = g_new(UserSettings,1);
+    // tu trafia funkcja z UI zajmujaca sie ustawieniem jakies wartosci z formularza czy czegos takiego.
+    user->age =  25;
+
+
+    
+    ram_store_save("user_settings", user);
+}
+
+void initialize_data(){
+    init_memory();
+    on_save_button_clicked();
+    
+    gpointer raw_data = g_hash_table_lookup(memory_collection,"user_settings");
+    UserSettings *new_user = (UserSettings *)raw_data;
+    printf("Age: %d\n",new_user->age);
+    g_hash_table_destroy(memory_collection);
+    printf("Memory cleard");
+}
