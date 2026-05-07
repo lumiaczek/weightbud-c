f10119215bd76552264ee62f239542fdbab1b34a
Daniel Borowski <danielborowski05@gmail.com>
2026-05-07 15:45:38 +0200

    Dodano ikonki, podmieniono trochę styli, głównie zmiany kosmetyczne

--- src/gui.c
+++ src/gui.c
@@ -1,9 +1,7 @@
 #include "gui.h"
+#include "events.h"
 #include <fontconfig/fontconfig.h>
 #include <gtk/gtk.h>
-#include "events.h"
-
-
 
 static void load_css(void) {
     GtkCssProvider *provider = gtk_css_provider_new();
@@ -11,21 +9,17 @@ static void load_css(void) {
     GdkScreen *screen = gdk_display_get_default_screen(display);
 
     gtk_css_provider_load_from_path(provider, "assets/style.css", NULL);
-
     gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
     g_object_unref(provider);
 }
 
 static void load_custom_fonts(void) {
     const FcChar8 *font_path1 = (const FcChar8 *)"assets/fonts/Montserrat-VariableFont_wght.ttf";
-
     const FcChar8 *font_path2 = (const FcChar8 *)"assets/fonts/Bungee-Regular.ttf";
 
     if (!FcConfigAppFontAddFile(FcConfigGetCurrent(), font_path1)) {
         g_printerr("Nie udało się załadować czcionki z: %s\n", font_path1);
     }
-
     if (!FcConfigAppFontAddFile(FcConfigGetCurrent(), font_path2)) {
         g_printerr("Nie udało się załadować czcionki z: %s\n", font_path2);
     }
@@ -48,16 +42,22 @@ static void load_svg_icon(GtkBuilder *builder, const char *image_id, const char 
     }
 }
 
+void enforce_numeric_input(GtkEditable *editable, const gchar *new_text, gint new_text_length, gint *position, gpointer user_data) {
+    for (int i = 0; i < new_text_length; i++) {
+        if (!g_ascii_isdigit(new_text[i]) && new_text[i] != '.' && new_text[i] != ',') {
+            g_signal_stop_emission_by_name(G_OBJECT(editable), "insert-text");
+            return;
+        }
+    }
+}
+
 void on_app_activate(GtkApplication *app, gpointer user_data) {
     load_custom_fonts();
     load_css();
 
-    GtkIconTheme *icon_theme = gtk_icon_theme_get_default();
-    gtk_icon_theme_append_search_path(icon_theme, "assets/icons");
-
-    GtkBuilder *builder = gtk_builder_new_from_file("gui.ui");
+    GtkBuilder *builder = gtk_builder_new_from_file("src/gui.ui");
     GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
-    
+
     gtk_application_add_window(app, GTK_WINDOW(window));
 
     GtkWidget *hero_image = GTK_WIDGET(gtk_builder_get_object(builder, "hero_image"));
@@ -72,12 +72,32 @@ void on_app_activate(GtkApplication *app, gpointer user_data) {
         g_error_free(error);
     }
 
-    GtkWidget *stack = GTK_WIDGET(gtk_builder_get_object(builder,"main_stack"));
-    GtkWidget *btn_start = GTK_WIDGET(gtk_builder_get_object(builder,"btn_start"));
-    
-    g_signal_connect(btn_start,"clicked",G_CALLBACK(change_view_on_click),stack);
+    GtkWidget *stack = GTK_WIDGET(gtk_builder_get_object(builder, "main_stack"));
+    GtkWidget *btn_start = GTK_WIDGET(gtk_builder_get_object(builder, "btn_start"));
 
+    g_signal_connect(btn_start, "clicked", G_CALLBACK(change_view_on_click), stack);
 
+    const char *numeric_entries[] = {"entry_weight", "entry_height", "entry_target_weight", "entry_target_muscle", "entry_target_fat"};
+    for (int i = 0; i < 5; i++) {
+        GtkWidget *entry = GTK_WIDGET(gtk_builder_get_object(builder, numeric_entries[i]));
+        if (entry) {
+            g_signal_connect(entry, "insert-text", G_CALLBACK(enforce_numeric_input), NULL);
+        }
+    }
+
+    load_svg_icon(builder, "icon_treningi", "assets/icons/dumbbell.svg", 64);
+    load_svg_icon(builder, "icon_waga", "assets/icons/biceps.svg", 64);
+    load_svg_icon(builder, "icon_suple", "assets/icons/pill.svg", 64);
+    load_svg_icon(builder, "icon_user", "assets/icons/user.svg", 32);
+    load_svg_icon(builder, "icon_target", "assets/icons/target.svg", 32);
+
+    load_svg_icon(builder, "icon_male", "assets/icons/mars.svg", 28);
+    load_svg_icon(builder, "icon_female", "assets/icons/venus.svg", 28);
+    load_svg_icon(builder, "icon_burn", "assets/icons/flame.svg", 28);
+    load_svg_icon(builder, "icon_scale", "assets/icons/scale.svg", 28);
+    load_svg_icon(builder, "icon_muscle_radio", "assets/icons/biceps.svg", 28);
+
+    load_svg_icon(builder, "icon_next_arrow", "assets/icons/move-right.svg", 28);
 
     g_object_unref(builder);
     gtk_widget_show_all(window);
